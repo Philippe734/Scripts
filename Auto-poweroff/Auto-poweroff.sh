bd54fb6cefa7498101859a260fe82e4cbd5ca81f
Philippe734 <philippe734@gmail.com>
2017-02-15 11:54:38 +0100

    Update Auto-poweroff.sh

--- Auto-poweroff/Auto-poweroff.sh
+++ Auto-poweroff/Auto-poweroff.sh
@@ -5,6 +5,9 @@
 # This script is supposed to be called from a cron job.
 # If you change this script's name/path, don't forget to update it in crontab !!
 
+# Required for notify-send to work
+eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)";
+
 level=$(cat /sys/class/power_supply/BAT1/capacity)
 status=$(cat /sys/class/power_supply/BAT1/status)
 
@@ -14,29 +17,27 @@ if [ "${status}" != "Discharging" ]; then
 fi
 
 
-# Source the environment variables required for notify-send to work.
-. /home/anmol/.env_vars
-
 # Percentage at which to show low-battery notification
-low_notif_percentage=20
+low_notif_percentage=14
 # Percentage at which to show critical-battery notification
-critical_notif_percentage=15
+critical_notif_percentage=11
 # Percentage at which to power-off
-critical_action_percentage=10
+action_percentage=8
 
 
-if [ "${level}" -le ${critical_action_percentage} ]; then
-  # sudo is required when running from cron
-  sudo systemctl hibernate
+if [ "${level}" -le ${action_percentage} ]; then  
+  notify-send "Warning, Linux will be poweroff because battery is too low: ${level}%" -t 15
+  sleep 5
+  systemctl poweroff
   exit 0
 fi
 
 if [ "${level}" -le ${critical_notif_percentage} ]; then
-  notify-send -i '/usr/share/icons/gnome/256x256/status/battery-caution.png' "Battery critical: ${level}%"
+  notify-send "Battery critical: ${level}%" -t 15
   exit 0
 fi
 
 if [ "${level}" -le ${low_notif_percentage} ]; then
-  notify-send -i '/usr/share/icons/gnome/256x256/status/battery-low.png' "Battery low: $level%"
+  notify-send "Battery low: $level%" -t 15
   exit 0
 fi
