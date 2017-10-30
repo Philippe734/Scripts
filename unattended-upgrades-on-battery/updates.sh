c8fb52eba621aca18380911f9db4a7fad4778962
Philippe734 <Philippe734@users.noreply.github.com>
2017-10-30 08:01:46 +0100

    Update updates.sh

--- unattended-upgrades-on-battery/updates.sh
+++ unattended-upgrades-on-battery/updates.sh
@@ -1,20 +1,24 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 # Run the unattended-upgrades if machine running on battery, perfect for laptop
 #
 # First ensure that you have to allow unattended-upgrades and apt update to be execute without sudo :
-#    whereis unattended-upgrades
-#    whereis apt
-#    sudo visudo -f /etc/sudoers.d/custom
-#    UserName ALL=NOPASSWD: /path of the command to/unattended-upgrades
-#    UserName ALL=NOPASSWD: /path of the command to/apt update
+# Requirement :
+#    Unattended-upgrades installed and enabled
+#    Allow unattended-upgrades and apt update to be execute without prompt password with this :
+#        whereis unattended-upgrades
+#        whereis apt
+#        sudo visudo -f /etc/sudoers.d/custom
+#        UserName ALL=NOPASSWD: /path of the command to/unattended-upgrades
+#        UserName ALL=NOPASSWD: /path of the command to/apt update
 #
-# Then put the script to be start at login
+# Set this script to be started at login
 #
 # Tested succesfull on Ubuntu 14.04 and 16.04
 # This script is supposed to be start at login
+# Philippe734 - 2017
 
-# Start of the script
+# Begin of the script
 
 # Run unattended-upgrades on battery if > 70% after 6 minutes
 sleep 6m
