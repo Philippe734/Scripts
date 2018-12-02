1b6c786df9f34586e1d762d08506475390f11a91
Philippe734 <Philippe734@users.noreply.github.com>
2018-12-02 16:44:36 +0100

    Update suspend.when.battery.low.sh

--- Suspend.when.battery.low/suspend.when.battery.low.sh
+++ Suspend.when.battery.low/suspend.when.battery.low.sh
@@ -1,60 +1,33 @@
 #!/bin/bash
-
 # Notifies the user if the battery is low then supsend when critical.
+# Usefull script when Ubuntu fail to suspend the laptop.
 # You can replace suspend with shutdown or hiberbate.
-#
-# First ensure that you can suspend non-interactively from cron without sudo :
-# Execute : sudo visudo -f /etc/sudoers.d/custom
-# Enter the following into the buffer and saved it :
-#     # Enable suspend from cron
-#     YourUserLogin ALL=NOPASSWD: /bin/systemctl suspend
-#
-# Then, schedule it via cron :
-#    chmod +x auto-poweroff.sh
-#    sudo crontab -e
-#    Add at the enf to execute every minute :
-#    * * * * * /path/to/auto-poweroff.sh.
-#
-# Tested succesfull on Ubuntu Mate 16.04 x64 with ASUS computer.
-# This script is supposed to be called from a cron job.
-# If you change this script's name/path, don't forget to update it in crontab.
-# Credit to Anmol-Singh-Jaggi on GitHub
+# This script is supposed to be start at login using "Startup Applications Preferences"
+# Infinite loop which check battery level every minute
 
-# Required for notify-send to work
-eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)";
+while [ "true" ]
+do
+	sleep 1m
+    # BAT0 with Ubuntu 16.04+
+    level=$(cat /sys/class/power_supply/BAT0/capacity)
+    status=$(cat /sys/class/power_supply/BAT0/status)
 
-# BAT0 with Ubuntu 16.04
-level=$(cat /sys/class/power_supply/BAT0/capacity)
-status=$(cat /sys/class/power_supply/BAT0/status)
+    # If discharging
+    if [ "${status}" = "Discharging" ]; then
+    
+        # Percentage at which to show critical-battery notification
+        critical_notif_percentage=10
 
-# Exit if not discharging
-if [ "${status}" != "Discharging" ]; then
-  exit 0
-fi
+        # Percentage at which to suspend
+        action_percentage=6
 
-
-# Percentage at which to show low-battery notification
-low_notif_percentage=14
-# Percentage at which to show critical-battery notification
-critical_notif_percentage=11
-# Percentage at which to power-off
-action_percentage=8
-
-
-if [ "${level}" -le ${action_percentage} ]; then  
-  notify-send "Warning, Linux will be suspend because battery is too low: ${level}%" -t 15
-  sleep 5
-  # Sudo is required when running from cron
-  sudo systemctl suspend
-  exit 0
-fi
-
-if [ "${level}" -le ${critical_notif_percentage} ]; then
-  notify-send "Battery critical: ${level}%" -t 15
-  exit 0
-fi
-
-if [ "${level}" -le ${low_notif_percentage} ]; then
-  notify-send "Battery low: $level%" -t 15
-  exit 0
-fi
+        if [ "${level}" -le ${action_percentage} ]; then  
+          notify-send "Standby in 5 seconds, battery too low"
+          sleep 5
+          systemctl suspend
+        elif [ "${level}" -le ${critical_notif_percentage} ]; then
+          notify-send "Low battery"
+        fi
+    fi
+done
+exit 0
