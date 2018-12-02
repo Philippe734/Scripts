126a39233c104dc3f19d505448a02ba4c17e138e
Philippe734 <Philippe734@users.noreply.github.com>
2018-12-02 16:46:52 +0100

    Update updates.sh

--- updates.silently.for.laptop/updates.sh
+++ updates.silently.for.laptop/updates.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Run updates automatically and silently on machine running on battery, perfect for laptop.
 # Also repair and clean packages.
 #
@@ -9,11 +8,9 @@
 #        UserName ALL=NOPASSWD: /path to this/script.sh
 #
 # Tested succesfull on Ubuntu 14.04, 16.04 and 18.04
-# This script is supposed to be start at login and run with sudo
+# This script is supposed to be start at login using "Startup Applications Preferences" run with sudo
 # Philippe734 - 2017
-
-# Begin of the script
-
+#
 # Run updates on battery if > 80% at login
 sleep 1m
 
@@ -32,5 +29,4 @@ if [ "${level}" -ge 80 ]; then
 	doupdate () { (sudo apt update ; sudo dpkg --configure -a ; sudo apt-get install -fy ; sudo apt-get autoclean ; sudo apt-get autoremove --purge -y ; sudo apt full-upgrade -y) > /dev/null; quit ; }
 	doupdate | yad --notification --no-middle --text="$MSG" --image="system-software-update" --command="yad --center --image=dialog-information --text=\"$MSG\" --button=OK" --listen
 fi
-
 exit 0
