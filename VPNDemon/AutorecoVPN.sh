1315bb15b5d67fa9b7d75e274c71f5116622af68
Philippe734 <Philippe734@users.noreply.github.com>
2019-05-01 19:17:59 +0200

    Update AutorecoVPN.sh

--- VPNDemon/AutorecoVPN.sh
+++ VPNDemon/AutorecoVPN.sh
@@ -3,7 +3,7 @@
 # étape 2 : saisir le UUI dans ce script et DecoVPN.sh
 
 TargetProgramName="qbittorrent"
-VPNUUID="de8b0af1-efce-40b4-871b-9f1d544cc4ac" # <<<<<<<< modifier ce UUI
+VPNUUID="xxxxxxxxxxxx" # <<<<<<<< modifier ce UUI
 #notify-send "Connexion du VPN..."
 exec /ThePathTo/vpndemon.sh &
 while [ "true" ]
