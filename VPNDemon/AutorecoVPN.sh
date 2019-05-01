ad3297c44583319101f45b617e62f4d563ba654c
Philippe734 <Philippe734@users.noreply.github.com>
2019-05-01 19:17:46 +0200

    Update AutorecoVPN.sh

--- VPNDemon/AutorecoVPN.sh
+++ VPNDemon/AutorecoVPN.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-# étape 1 : récupérer le uui du vpn : nmcli con
-# étape 2 : saisir le uui dans ce script
-# étape 3 : saisir son nom dans ce script
+# étape 1 : récupérer le UUI du vpn : nmcli con
+# étape 2 : saisir le UUI dans ce script et DecoVPN.sh
+
 TargetProgramName="qbittorrent"
-notify-send "Connexion du VPN..."
+VPNUUID="de8b0af1-efce-40b4-871b-9f1d544cc4ac" # <<<<<<<< modifier ce UUI
+#notify-send "Connexion du VPN..."
 exec /ThePathTo/vpndemon.sh &
 while [ "true" ]
 do
-	VPNCON=$(nmcli con status)
-	if [[ $VPNCON != *openvpn3* ]]; then
+	VPNCON=$(nmcli con show --active | grep "$VPNUUID")
+	if [ -z "$VPNCON" ]; then
 		echo "VPN déconnecté"
-		(sleep 1s && nmcli con up uuid xxxx-xx-xx-xx-xxxxx)
+		(sleep 1s && nmcli con up uuid "$VPNUUID") 
 		FLAGRECO="vrai"
 	else
 		if [[ $FLAGRECO = "vrai" ]]; then
 			FLAGRECO="faux"
-			exec $TargetProgramName &
+			#exec $TargetProgramName &
 		fi
 	fi
 	sleep 10
