794207e5529760014f1f29fa99cfdb0d6af33dea
Philippe734 <Philippe734@users.noreply.github.com>
2019-05-01 19:19:19 +0200

    Update DecoVPN.sh

--- VPNDemon/DecoVPN.sh
+++ VPNDemon/DecoVPN.sh
@@ -8,5 +8,5 @@ sleep 2
 notify-send "Déconnexion..."
 pkill AutorecoVPN.sh
 sleep 2
-nmcli con down uuid xxx-xx-xx-xx-xxxxx
+nmcli con down uuid xxx-xx-xx-xx-xxxxx <<<<<<<<<<< remplacer xxx par UUID du VPN
 notify-send "VPN déconnecté"
