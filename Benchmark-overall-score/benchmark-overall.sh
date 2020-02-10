3a69b5c2284dde5acf6176b6e735d11bb6fd1692
Philippe734 <Philippe734@users.noreply.github.com>
2020-02-10 16:13:23 +0100

    Add files via upload

---
+++ Benchmark-overall-score/benchmark-overall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Benchmark with sysbench and calculate a single overall score
+# CPU, Memory & Disk IO
+# 2020 - Philippe734
+
+echo "Starting with cpu, open your system monitor to watch..."
+cpu=$(sysbench --test=cpu --cpu-max-prime=20000 --num-threads=32 run 2>/dev/null | grep "total number of events" | cut -f2 -d ":") 
+
+echo "Disk IO..."
+sysbench --test=fileio --file-total-size=2G prepare >/dev/null 2>&1
+
+sleep 1s
+io=$(sysbench --test=fileio --file-total-size=2G --file-test-mode=rndrw --max-time=180 --max-requests=0 run 2>/dev/null | grep "total number of events" | cut -f2 -d ":") 
+sleep 1s
+
+sysbench --test=fileio --file-total-size=2G cleanup >/dev/null 2>&1
+
+echo "Memory..."
+mem=$(sysbench --test=memory --num-threads=32 run 2>/dev/null | grep "total number of events" | cut -f2 -d ":")
+
+echo "Done, overall score:"
+echo "$((($cpu*$mem*3*$io*5)/(10**12)))"
