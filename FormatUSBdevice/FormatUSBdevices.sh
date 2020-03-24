ba41512021418a447515ca6026ce36a03d2fee05
Philippe734 <Philippe734@users.noreply.github.com>
2020-03-24 10:45:28 +0100

    Update FormatUSBdevices.sh

--- FormatUSBdevice/FormatUSBdevices.sh
+++ FormatUSBdevice/FormatUSBdevices.sh
@@ -11,7 +11,7 @@ REMOVABLE_DRIVES=""
 for _device in /sys/block/*/device; do
     if echo $(readlink -f "$_device")|egrep -q "usb"; then
         _disk=$(echo "$_device" | cut -f4 -d/)
-		_info=$(lsblk -rno NAME,SIZE,MOUNTPOINT /dev/"$_disk"1)
+		_info=$(lsblk -rno NAME,SIZE,MOUNTPOINT,LABEL /dev/"$_disk"1)
         REMOVABLE_DRIVES="$_info!$REMOVABLE_DRIVES"
     fi
 done
@@ -23,12 +23,18 @@ _media=$(echo $_out | cut -f1 -d'|')
 _filesystem=$(echo $_out | cut -f2 -d'|')
 _name=$(echo $_out | cut -f3 -d'|')
 
+#echo "drive : $_drive"
+#echo "_media : $_media"
+#echo "_filesystem : $_filesystem"
+#echo "_name : $_name"
+
+
 if [ -z "$_media" -o "$_media" == "||" ]; then
     echo "canceled"
     exit 0
 fi
 
-yad --center  --image "dialog-question" --title "Attention" --button=No:0 --button=Yes:1 --text "Are you sure to format this device?\n$_media"
+yad --center  --image "dialog-question" --title "Attention" --button=No:0 --button=Yes:1 --text "Are you sure to format this device?\n$_media" --fixed --width=300 --height=70
 
 if [ $? = 1 ]; then
     umount /dev/"$_drive"
@@ -37,6 +43,9 @@ if [ $? = 1 ]; then
         "NTFS") _filesystem="mkfs.ntfs -f -L";;
         "EXT4") _filesystem="mkfs.ext4 -L";;
     esac
+    #echo "filesystem : $_filesystem"
+    #echo "drive : $_drive"
+    #echo "COMMAND : $_filesystem $_name /dev/$_drive"
 	$_filesystem "$_name" /dev/"$_drive" | yad --progress --pulsate --text="Please wait while formating..." --auto-close --width=300 --height=70 --fixed --undecorated --center --no-buttons --progress-text=""
 	echo "formated"
 else
