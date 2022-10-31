c5213053f5d5ea1e437444191bb7e226797fe62d
Philippe734 <Philippe734@users.noreply.github.com>
2022-10-31 07:11:11 +0100

    Update AutorecoVPN.sh

--- VPNDemon/AutorecoVPN.sh
+++ VPNDemon/AutorecoVPN.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 # étape 1 : récupérer le UUI du vpn : nmcli con
-# étape 2 : saisir le UUI dans ce script et DecoVPN.sh
+# étape 2 : saisir le UUI dans ce script, DecoVPN.sh et AutoDeco3h.sh
 
 TargetProgramName="qbittorrent"
-VPNUUID="xxxxxxxxxxxx" # <<<<<<<< modifier ce UUI
-#notify-send "Connexion du VPN..."
-exec /ThePathTo/vpndemon.sh &
+
+VPNUUID="..." # UUI pour torrent
+VPNFRANCE="..." # UUI VPN France
+
+# Déconnexion du VPN France
+nmcli con down uuid "$VPNFRANCE" # UUI VPN France
+sleep 500ms
+
+# Activer le monitoring
+exec /opt/scripts/VPN/vpndemon.sh &
+
+# Autodéco périodiquement
+exec /opt/scripts/VPN/AutoDeco3h.sh &
+
 while [ "true" ]
 do
 	VPNCON=$(nmcli con show --active | grep "$VPNUUID")
@@ -16,8 +27,8 @@ do
 	else
 		if [[ $FLAGRECO = "vrai" ]]; then
 			FLAGRECO="faux"
-			#exec $TargetProgramName &
+			exec $TargetProgramName &
 		fi
 	fi
-	sleep 10
+	sleep 3
 done
