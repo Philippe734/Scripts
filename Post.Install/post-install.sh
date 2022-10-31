765f9383b1987486af72d5932e8e876c9a0b9d3d
Philippe734 <Philippe734@users.noreply.github.com>
2022-10-31 07:03:44 +0100

    Update post-install.sh

--- Post.Install/post-install.sh
+++ Post.Install/post-install.sh
@@ -3,28 +3,15 @@
 # Philippe - 2019
 # Script à déposer sur mes clés USB
 # Récupère et exécute les scripts post-install pour sudo et sans sudo
-# Pour l'exécuter, utiliser: bash /chemin/NomScript.sh
+# Pour l'exécuter, utiliser: bash /chemin/post-install.sh
 
 FILESUDO=~/post-install-sudo.sh
 FILEUSER=~/post-install-user.sh
-(
-while [ ! -x "$FILESUDO" ]; do
-    rm $FILESUDO
-    wget https://www.dropbox.com/s/mydropboxID/post-install-sudo.sh -P ~
-    sleep 1s
-    chmod +x $FILESUDO
-done
-while [ ! -x "$FILEUSER" ]; do
-    rm $FILEUSER
-    wget https://www.dropbox.com/s/mydropboxID/post-install-user.sh -P ~
-    sleep 1s
-    chmod +x $FILEUSER
-done
-) | zenity --progress --pulsate --width=300 --text="Initialisation..." --title="Post-install" --no-cancel --auto-close
+chmod +x $FILESUDO
+chmod +x $FILEUSER
 
 bash $FILEUSER &
 
 pkexec env DESKTOP_SESSION=$DESKTOP_SESSION DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY bash $FILESUDO
 
 # ne pas terminer avec exit 0
-
