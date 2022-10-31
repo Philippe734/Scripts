8fd8506da5f5f76e83d3f4d2a2063f1eb54e365a
Philippe734 <Philippe734@users.noreply.github.com>
2022-10-31 07:05:15 +0100

    Update post-install-user.sh

--- Post.Install/post-install-user.sh
+++ Post.Install/post-install-user.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 # Opérations : post installation pour Ubuntu Mate & Xubuntu
 # Philippe734 - 2019
-# Récupérer ce script: wget https://www.dropbox.com/s/mydropboxID/post-install-user.sh -P ~
 # Ce script est uniquement sans sudo
 # Fonctions avec sudo > utiliser le script post-install-sudo.sh
-
-# pour tester :
-
-#echo "Test terminé"
-#exit 0
 
 # Laisser la TODOlist dans sudo car faut installer yad
 
@@ -25,28 +19,27 @@ wget https://www.dropbox.com/s/3loxh983wtihc9r/mozo-made.desktop -P ~
 echo "30" ; sleep 0.3
 mv ~/mozo-made.desktop ~/.local/share/applications/lock-panel.desktop
 
+# Raccourcis vers l'appli de dépannage
+wget https://www.dropbox.com/s/n28ywniaodd9s15/depannage.desktop -P ~
+mkdir --p ~/.local/share/applications
+mv ~/depannage.desktop ~/.local/share/applications/depannage.desktop
+
+# Fonds d'écran Windows 10
 wget https://www.dropbox.com/s/2zp5li0p665nyd7/Wallpaper-windows10.jpg -P ~/"Images/Fonds d'écran"
 echo "35" ; sleep 0.3
 
-
-# correction du thème pour LibreOffice
-# car l'apparence de LibreOffice est défectueuse avec le thème Materia
-specifictheme="Clearlooks"
-find /usr/share/applications -name libreoffice* | xargs -i cp {} ~/.local/share/applications
-find ~/.local/share/applications -name libreoffice* | xargs sed -i "s/Exec=/Exec=env GTK_THEME=$specifictheme /g"
-echo "50" ; sleep 0.3
-
-# icones Windows 10
-FILE=~/2NcMwiu
-while [ ! -f "$FILE" ]; do
-    rm $FILE
-    wget https://bit.ly/2NcMwiu -P ~
-    sleep 1s
-done
-echo "80" ; sleep 0.3
-unzip ~/2NcMwiu -d ~/.icons
-echo "99" ; sleep 2s
+echo "99" ; sleep 1s
 echo "100"
 ) | zenity --progress --title="Paramètres" --width=300 --no-cancel --auto-close
+
+# Icones Windows 10
+FILE=~/icon-win10.zip
+while [ ! -f "$FILE" ]; do
+    rm $FILE
+    wget https://www.dropbox.com/s/a7n2vpl2ylhyygu/icon-win10.zip -P ~
+    sleep 3s
+done
+sleep 0.3
+unzip "$FILE" -d ~/.icons
 
 # ne pas terminer avec exit 0
