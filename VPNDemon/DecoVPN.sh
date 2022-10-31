aea01a2f2a7320ebb1d071daf68e521b97c6fa23
Philippe734 <Philippe734@users.noreply.github.com>
2022-10-31 07:11:27 +0100

    Update DecoVPN.sh

--- VPNDemon/DecoVPN.sh
+++ VPNDemon/DecoVPN.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 KillProgName="qbittorrent"
-notify-send "Fermeture des torrents..."
+#notify-send "Fermeture des torrents..."
 pkill $KillProgName
 pkill $KillProgName
 pkill $KillProgName
-sleep 2
-notify-send "Déconnexion..."
+sleep 1
+#notify-send "Déconnexion..."
 pkill AutorecoVPN.sh
-sleep 2
-nmcli con down uuid xxx-xx-xx-xx-xxxxx <<<<<<<<<<< remplacer xxx par UUID du VPN
-notify-send "VPN déconnecté"
+pkill AutoDeco3h.sh
+sleep 1
+
+# déconnexion du VPN torrent
+nmcli con down uuid ... # UUI pour torrent
+
+#nmcli con up ... # Ethernet
+
+# connexion du VPN France
+nmcli con up ... # UUI du VPN France
+
