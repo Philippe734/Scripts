ef3953b5d2ff03426d5d2d4916b9148d5515f5ff
Philippe734 <Philippe734@users.noreply.github.com>
2023-02-23 19:25:35 +0100

    Update BrightnessSystray.sh

--- BrightnessSystray/BrightnessSystray.sh
+++ BrightnessSystray/BrightnessSystray.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 # Script to add an icon in the notification area, 
-# to set the brightness of the screen
+# to set the screen's brightness, by right clic
 
 # Require yad : sudo apt install yad
 
+get_active_monitors()
+{
+    xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}'
+}
+
 # Name of the screen
-DisplayName='DisplayPort-1'
+DisplayName=$(get_active_monitors)
 
 # set by default
 xrandr --output "$DisplayName" --brightness 0.75
 
 # set brightness
-yad --notification --text="Select the brightness" --image="/Path/To/Icon.png" --menu='Set to 100%!xrandr --output '$DisplayName' --brightness 1|Set to 95%!xrandr --output '$DisplayName' --brightness 0.95|Set to 90%!xrandr --output '$DisplayName' --brightness 0.9|Set to 85%!xrandr --output '$DisplayName' --brightness 0.85|Set to 80%!xrandr --output '$DisplayName' --brightness 0.8|Set to 75%!xrandr --output '$DisplayName' --brightness 0.75|Set to 70%!xrandr --output '$DisplayName' --brightness 0.70|Set to 65%!xrandr --output '$DisplayName' --brightness 0.65|Set to 60%!xrandr --output '$DisplayName' --brightness 0.6|Set to 55%!xrandr --output '$DisplayName' --brightness 0.55|Set to 50%!xrandr --output '$DisplayName' --brightness 0.5|Set to 45%!xrandr --output '$DisplayName' --brightness 0.45|Set to 40%!xrandr --output '$DisplayName' --brightness 0.4|Set to 35%!xrandr --output '$DisplayName' --brightness 0.35|Set to 30%!xrandr --output '$DisplayName' --brightness 0.3|Quit!quit' --command="" --no-middle
+yad --notification --text="Select the brightness" --image="/Path/To/Icon.png" --menu='Set the brightness|100%!xrandr --output '$DisplayName' --brightness 1|95%!xrandr --output '$DisplayName' --brightness 0.95|90%!xrandr --output '$DisplayName' --brightness 0.9|85%!xrandr --output '$DisplayName' --brightness 0.85|80%!xrandr --output '$DisplayName' --brightness 0.8|75%!xrandr --output '$DisplayName' --brightness 0.75|70%!xrandr --output '$DisplayName' --brightness 0.70|65%!xrandr --output '$DisplayName' --brightness 0.65|60%!xrandr --output '$DisplayName' --brightness 0.6|55%!xrandr --output '$DisplayName' --brightness 0.55|50%!xrandr --output '$DisplayName' --brightness 0.5|45%!xrandr --output '$DisplayName' --brightness 0.45|40%!xrandr --output '$DisplayName' --brightness 0.4|35%!xrandr --output '$DisplayName' --brightness 0.35|30%!xrandr --output '$DisplayName' --brightness 0.3|Quit!quit' --command="" --no-middle
 
 exit 0
-
