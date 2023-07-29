ae1e41c7e057f92fd53c92bde47f9fa800bd12d1
Philippe734 <Philippe734@users.noreply.github.com>
2023-07-29 09:50:32 +0200

    Update psw_protec.sh

--- password_protect_app/psw_protec.sh
+++ password_protect_app/psw_protec.sh
@@ -2,17 +2,19 @@
 
 # Simple password protection for application
 # 2023 - Philippe734
-# It ask password then run application
+# Prompt for password then run application
 #
 # Require yad:
 # $ sudo apt install yad
 #
-# Before use it, use these command to get the full path of your application, encrypted and coded in base64, with your password:
-# $ echo -n "/Path/To/Your/Application" | openssl enc -aes256 -a -pbkdf2
+# Before use it, hide the application's file in your system, then use these commands to get the full path of your application, encrypted and coded in base64, with your password:
+# $ password=$(yad --text-align=center --text="Password" --entry --entry-text="" --hide-text --fixed --title="" --button=OK)
+# $ data="/Path/To/Your/Frame.AppImage"
+# $ echo "$data" | openssl enc -aes256 -a -pbkdf2 -pass pass:"$password"
 
 function main
 {
-password=$(yad --text-align=center --text="Password" --entry --entry-text="" --hide-text --fixed --title="" --button=OK)
+password=$(yad --title="Application's name" --height=150 --width=400 --hide-text --fixed --text="<span foreground='blue'><b><big><big>Please enter your password</big></big></b></span>" --entry --entry-text="" --text-align=center --center --borders=20 --image='/home/username/Pictures/application's icon.png')
 
 data="xxx" # replace xxx with the full path of your application, encrypted and coded in base64
 
